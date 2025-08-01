b9552c08cc158d2c5a270415785585fd5ed24ad6
Ben Lloyd <ben@ben-lloyd.com>
2025-08-01 15:09:58 +0100

    Fix for headless-gl running into code signing issues with ANGLE on macOS. Also solves issues arrising from it not supporting WebGL2, requiring a force to make it run WebGL 1. Got the code inline with company ESLint procedure and updated all the compatible node-modules.

--- test/claygl2.js
+++ test/claygl2.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const GIFEncoder = require('gifencoder');
-const clay = require('claygl');
-const {createCanvas} = require('../lib');
+const fs = require("fs");
+const GIFEncoder = require("gifencoder");
+const clay = require("claygl");
+const { createCanvas } = require("../lib");
 
 const width = 512,
-  height = 512;
+	height = 512;
 
 const canvas = createCanvas(width, height);
 
@@ -14,44 +14,46 @@ let idx = 0;
 
 // polyfill
 global.document = {
-  createElement() {
-    return createCanvas(300, 150);
-  },
+	createElement() {
+		return createCanvas(300, 150);
+	}
 };
 
 clay.application.create(canvas, {
 
-  width,
-  height,
+	width,
+	height,
 
-  init(app) {
-    // Create camera
-    this._camera = app.createCamera([0, 2, 5], [0, 0, 0]);
+	init(app) {
+		// Create camera
+		this._camera = app.createCamera([0, 2, 5], [0, 0, 0]);
 
-    // Create a RED cube
-    this._cube = app.createCube({
-      color: [1, 0, 0],
-    });
+		// Create a RED cube
+		this._cube = app.createCube({
+			color: [1, 0, 0]
+		});
 
-    // Create light
-    this._mainLight = app.createDirectionalLight([-1, -1, -1]);
-    // app.createAmbientLight('#fff', 1.0);
+		// Create light
+		this._mainLight = app.createDirectionalLight([-1, -1, -1]);
+		// app.createAmbientLight('#fff', 1.0);
 
-    encoder.createReadStream().pipe(fs.createWriteStream('./snapshot/claygl-cube.gif'));
-    encoder.start();
-    encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
-    encoder.setDelay(16); // frame delay in ms
-    encoder.setQuality(10); // image quality. 10 is default.
-  },
-  loop(app) {
-    this._cube.rotation.rotateY(16 / 1000);
-    if(idx > 0) {
-      encoder.addFrame(canvas.__ctx__);
-      console.log(`add frame ${idx}`);
-    }
-    idx++;
-    if(idx > 197) {
-      process.exit(-1);
-    }
-  },
+		encoder.createReadStream().pipe(fs.createWriteStream("./snapshot/claygl-cube.gif"));
+		encoder.start();
+		encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
+		encoder.setDelay(16); // frame delay in ms
+		encoder.setQuality(10); // image quality. 10 is default.
+	},
+	loop(app) {
+		this._cube.rotation.rotateY(16 / 1000);
+		if(idx > 0) {
+			encoder.addFrame(canvas.__ctx__);
+			console.log(`add frame ${idx}`);
+		}
+		idx++;
+		if(idx > 197) {
+			encoder.finish();
+			console.log("GIF generation completed successfully");
+			process.exit(0);
+		}
+	}
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,66 @@
-const webpack = require('webpack');
-const path = require('path');
-const packageConfig = require('./package.json');
+const webpack = require("webpack");
+const path = require("path");
+const packageConfig = require("./package.json");
 
 function toCamelCase(str) {
-  return str.replace(/-([a-z])/ig, (s, p1) => p1.toUpperCase());
+	return str.replace(/-([a-z])/ig, (s, p1) => p1.toUpperCase());
 }
 
 module.exports = function (env = {}) {
-  return {
-    mode: env.mode || 'none',
-    entry: './src/index',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: `${packageConfig.name}.js`,
-      publicPath: '/js/',
-      library: [toCamelCase(packageConfig.name)],
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      globalObject: 'this',
-    },
-    // resolve: {
-    //
-    // },
+	return {
+		mode: env.mode || "none",
+		entry: "./src/index",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: `${packageConfig.name}.js`,
+			publicPath: "/js/",
+			library: [toCamelCase(packageConfig.name)],
+			libraryTarget: "umd",
+			libraryExport: "default",
+			globalObject: "this"
+		},
+		// resolve: {
+		//
+		// },
 
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules\/.*/,
-          use: {
-            loader: 'babel-loader',
-            options: {babelrc: true},
-          },
-        },
-      ],
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules\/.*/,
+					use: {
+						loader: "babel-loader",
+						options: { babelrc: true }
+					}
+				}
+			]
 
-      /* Advanced module configuration (click to show) */
-    },
+			/* Advanced module configuration (click to show) */
+		},
 
-    externals: {
+		externals: {
 
-    },
-    // Don't follow/bundle these modules, but request them at runtime from the environment
+		},
+		// Don't follow/bundle these modules, but request them at runtime from the environment
 
-    stats: 'errors-only',
-    // lets you precisely control what bundle information gets displayed
+		stats: "errors-only",
+		// lets you precisely control what bundle information gets displayed
 
-    devServer: {
-      contentBase: path.join(__dirname, env.server || '.'),
-      compress: true,
-      port: 3000,
-      hot: true,
-      // ...
-    },
+		devServer: {
+			contentBase: path.join(__dirname, env.server || "."),
+			compress: true,
+			port: 3000,
+			hot: true
+			// ...
+		},
 
-    plugins: [
-      new webpack.HotModuleReplacementPlugin({
-        multiStep: true,
-      }),
-    ],
-    // list of additional plugins
+		plugins: [
+			new webpack.HotModuleReplacementPlugin({
+				multiStep: true
+			})
+		]
+		// list of additional plugins
 
-    /* Advanced configuration (click to show) */
-  };
+		/* Advanced configuration (click to show) */
+	};
 };
